e90423c473b4cbd6e6251c4b1370a3f46496c581
Victor Stern <victor.a.stern@gmail.com>
2023-09-12 11:30:41 +0200

    Co-authored-by: Linus Särud <zulln@users.noreply.github.com> Co-authored-by: Eric Dannetun <Elrokaren@users.noreply.github.com> Co-authored-by: josudden <josudden@users.noreply.github.com>

--- Project1.py
+++ Project1.py
@@ -1,8 +1,16 @@
 import numpy as np
-from numpy import *; from matplotlib.pyplot import *
 
 import pickle
 import gzip
+
+global test_inputs
+global test_targets
+
+def softmax(x, derivative):
+    exps = np.exp(x)
+    if derivative == True:
+        return exps/np.sum(exps,axis=0) * (1-exps/np.sum(exps,axis=0))
+    return exps/np.sum(exps,axis=0)
 
 # Define the sigmoid activation function
 def sigmoid(x):
@@ -14,28 +22,27 @@ def sigmoid_derivative(x):
 
 class feedForwardNN:
     def __init__(self, nbr_input, nbr_hidden, nbr_output):
-
         # Initialize the weights with random values
-        self.weights_input_hidden = np.random.rand(nbr_input, nbr_hidden);
-        self.weights_hidden_output = np.random.rand(nbr_hidden, nbr_output);
+        self.weights_input_hidden = np.random.randn(nbr_input, nbr_hidden)/np.sqrt(nbr_hidden)
+        self.weights_hidden_output = np.random.randn(nbr_hidden, nbr_output)/np.sqrt(nbr_output)
 
-        self.b_hid = 0;
-        self.b_out = 0;
+        self.b_hid = 0
+        self.b_out = 0
     
     def feedForward(self, input):
         self.hidden_activations = sigmoid(np.dot(input, self.weights_input_hidden) + self.b_hid)
-        self.output_activations = sigmoid(np.dot(self.hidden_activations, self.weights_hidden_output) + self.b_out)
+        self.output_activations = (np.dot(self.hidden_activations, self.weights_hidden_output) + self.b_out)
         
-        return self.output_activations
+        return softmax(self.output_activations,False)
     
     def backward(self, inputs, targets, learning_rate):
          # Calculate the error at the output layer
-        target = np.zeros(10);
-        target[targets] = 1;
+        target = np.zeros(10)
+        target[targets] = 1
 
-        output_error = sum(target - self.output_activations)
+        output_error = target - softmax(self.output_activations,False)
         # Calculate the gradient at the output layer using the error and the derivative of sigmoid
-        output_delta = output_error * sigmoid_derivative(self.output_activations)
+        output_delta = output_error * softmax(self.output_activations,True)
 
         # Calculate the error at the hidden layer
         hidden_error = output_delta.dot(self.weights_hidden_output.T)
@@ -43,11 +50,11 @@ class feedForwardNN:
         hidden_delta = hidden_error * sigmoid_derivative(self.hidden_activations)
 
         # Update the weights for both layers
-        weights_output_derivative = np.outer(self.hidden_activations.T, output_delta) * learning_rate
-        weights_input_derivative = np.outer(inputs.T,(hidden_delta)) * learning_rate
+        weights_output_derivative = np.outer(self.hidden_activations.T, output_delta)
+        weights_input_derivative = np.outer(inputs.T,(hidden_delta))
         # Update bias terms
-        b_out = np.sum(output_delta, axis=0) * learning_rate
-        b_hid = np.sum(hidden_delta, axis=0) * learning_rate
+        b_out = np.sum(output_delta, axis=0)
+        b_hid = np.sum(hidden_delta, axis=0)
 
         return [weights_output_derivative, weights_input_derivative, b_out, b_hid]
 
@@ -57,34 +64,26 @@ class feedForwardNN:
         self.weights_input_hidden = np.add(self.weights_input_hidden, (w_inputderivative / batch_size)*learning_rate)
         self.b_out = np.add(self.b_out, (b_outputderivative / batch_size)*learning_rate)
         self.b_hid = np.add(self.b_hid, (b_inputderivative / batch_size)*learning_rate)
-
-        #print(self.weights_hidden_output)
-        #print(self.weights_input_hidden)
     
     def stochGrad(self, inputs, targets, epochs, batch_size):
-        for _ in range(epochs):
-            index_vector = np.random.permutation(len(targets));
-            print(len(index_vector))
-            print(batch_size)
-            print(len(index_vector)/batch_size)
+        for epoch in range(1, epochs + 1):
+            index_vector = np.random.permutation(len(targets))
+            print(f"Starting epoch {epoch}...")
             
             for i in range(int(len(index_vector)/batch_size)):
                 # Forward pass
-                indices = index_vector[range(i*batch_size, (i+1)*batch_size)]; # Ta ut en batch
-                #batchImages = [inputs[n] for n in indices]
-                #batchTargets = [targets[n] for n in indices]
+                indices = index_vector[range(i*batch_size, (i+1)*batch_size)] # Ta ut en batch
                 
                 # from slides p 72, 1/(c * (1 + j)) where c > 0
-                #learning_rate = 1/(0.01 * (i + 1))
+                #learning_rate = 1/(2 * (i + 1))
                 learning_rate = 1
                 
-                predictions= []
                 w_outputderivative = np.zeros(self.weights_hidden_output.shape)
                 w_inputderivative = np.zeros(self.weights_input_hidden.shape)
                 b_outputderivative = 0
                 b_inputderivative = 0
                 for k in indices:
-                    predictions.append(self.feedForward(inputs[k])) # predicta bilder i batch
+                    self.feedForward(inputs[k]) # predicta bilder i batch
                     # Backpropagation
                     [dwout, dwhid, dbout, dbhid] = self.backward(inputs[k], targets[k], learning_rate)
                     w_outputderivative = np.add(w_outputderivative, dwout)
@@ -94,24 +93,29 @@ class feedForwardNN:
 
                 self.update_params(w_outputderivative, w_inputderivative, b_outputderivative, b_inputderivative, learning_rate, batch_size)
 
-    def test(self, test_inputs, test_targets):
+            self.test(test_inputs, test_targets, epoch)
+
+    def test(self, test_inputs, test_targets, epoch = 1):
         stat = 0;
         for i in range(len(test_inputs)):
             prediction = self.feedForward(test_inputs[i])
-            print(np.argmax(prediction))
             if np.argmax(prediction) == test_targets[i]: stat += 1
         
-        print(f"{stat/len(test_inputs) / 100}%, stat: {stat}")
+        print(f"Epoch: {epoch} \t{stat/len(test_inputs) * 100}%, total found: {stat}")
 
-network = feedForwardNN(784, 30, 10)
+def main():
+    network = feedForwardNN(784, 30, 10)
 
-with gzip.open("mnist.pkl.gz", 'rb') as f:
-    mnist = pickle.load(f, encoding="latin1")
+    with gzip.open("mnist.pkl.gz", 'rb') as f:
+        mnist = pickle.load(f, encoding="latin1")
 
-train_inputs = mnist[0][0]
-train_targets = mnist[0][1]
-test_inputs = mnist[1][0];
-test_targets = mnist[1][1];
+    train_inputs = mnist[0][0]
+    train_targets = mnist[0][1]
 
-network.stochGrad(train_inputs,train_targets,1,10)
-network.test(test_inputs,test_targets)
+    test_inputs = mnist[2][0]
+    test_targets = mnist[2][1]
+
+    network.stochGrad(train_inputs,train_targets,4,10)
+
+if __name__ == "__main__":
+    main()
