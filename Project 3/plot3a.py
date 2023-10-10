44ea04996730ebcb7dc3febee1b7cb9e589dcaca
Linus Särud <linus@sarud.se>
2023-10-10 12:20:11 +0200

    plot animation

--- Project 3/plot3a.py
+++ Project 3/plot3a.py
@@ -1,5 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
+
+def plotman(u1_list, u2_list, u3_list, u4_list, x_len, y_len):
+    fig, ax = plt.subplots()
+    
+    def update(frame):
+        ax.clear()
+        u1 = u1_list[frame].reshape((x_len, x_len))
+        u2 = u2_list[frame].reshape((y_len, x_len))
+        u3 = u3_list[frame].reshape((x_len, x_len))
+        u4 = u4_list[frame].reshape((int(x_len/2), int(x_len/2)))
+        
+        room_length = np.size(u1,1)
+        total_temp = np.zeros((room_length * 2, room_length * 3))
+        total_temp[room_length:, :room_length] = u1
+        total_temp[:, room_length:room_length*2] = u2
+        total_temp[:room_length, room_length*2:] = u3
+        b1 = int(room_length*3/2)
+        b2 = int(room_length*5/2)
+        total_temp[room_length:b1, room_length*2:b2] = u4
+        
+        masked_temp = np.ma.masked_where(total_temp == 0, total_temp)
+        
+        ax.imshow(masked_temp, cmap='hot', origin='lower', vmin=0, vmax=50)
+        ax.set_title('Temperature Distribution in Rooms')
+        ax.set_xlabel('X position')
+        ax.set_ylabel('Y position')
+        ax.invert_yaxis()
+        
+    ani = animation.FuncAnimation(fig, update, frames=len(u1_list), repeat=False)
+    ani.save('2frames.mp4', writer='ffmpeg')
+    plt.show()
+
 
 def plot3a(u1, u2, u3, u4, x_len, y_len):
     u1 = u1.reshape(   (x_len, x_len)  ) 
